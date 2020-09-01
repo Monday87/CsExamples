fba9f1c6ec81f85d1ad8ceb6e9030b88aafc966d
Steven Erixon <steven.erixon@hotmail.se>
2020-09-01 09:59:38 +0200

    C1

--- C1_WriteLine/Program.cs
+++ C1_WriteLine/Program.cs
@@ -6,7 +6,24 @@ namespace C1_WriteLine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string name = "Steven";
+            int age = 33;
+            var city = "Örebro";
+
+            
+
+            var text1 = "Welcome Steven. You are 33 years old right? Steven do you live in Örebro?";
+
+            var text2 = "Welcome "+ name +". You are "+ age +" years old right? "+ name +" do you live in "+ city +"?";
+            var text3 = String.Format("Welcome {0}. You are {1} years old right? {0} do you live in {2}?", name, age, city);
+            var text4 = $"Welcome {name}. You are {age} years old right? {name} do you live in {city}";
+
+            Console.WriteLine(text1);
+            Console.WriteLine(text2);
+            Console.WriteLine(text3);
+            Console.WriteLine(text4);
         }
-    }
 }
+}
+

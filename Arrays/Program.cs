379ce0f59fe6bc3d071ebb521fa8c481869cd5f8
Steven Erixon <steven.erixon@hotmail.se>
2020-09-01 15:39:24 +0200

    C8

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,7 +6,48 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("How many siblings do you have? ");
+            var _numberOfSiblings = Convert.ToInt32(Console.ReadLine());
+
+            if (_numberOfSiblings < 1)
+                Console.WriteLine("Okay. That's cool man.");
+            else
+            {
+
+                string[] siblings = new string[_numberOfSiblings];
+
+                if (_numberOfSiblings == 1)
+                {
+                    Console.WriteLine("What is your siblings name? ");
+                    siblings[0] = Console.ReadLine();
+
+                }
+                else 
+                {
+                    for (var i=0; i < _numberOfSiblings; i++)
+                    {
+                        if (i == 0)
+                            Console.Write("What is your first siblings name? ");
+                        else
+                            Console.Write("And your next siblings name is? ");
+
+                        siblings[i] = Console.ReadLine();
+                    }
+                }
+                 
+                if (_numberOfSiblings == 1)
+                   Console.Write("So correct me if I am wrong. Your sibling is ");
+                else
+                    Console.Write("So correct me if I am wrong. Your siblings are ");
+
+                foreach (var sibling in siblings)
+                {
+                    Console.Write($"{sibling} ");
+                
+                }
+
+            }
+
         }
     }
 }

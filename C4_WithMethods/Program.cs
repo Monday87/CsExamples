17b4978e33f3377221ec73098300477eb6e4580e
Steven Erixon <steven.erixon@hotmail.se>
2020-09-01 13:54:51 +0200

    C6

---
+++ C4_WithMethods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C4_WithMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            person.AskForName();
+            person.AskForAge();
+            person.AskForAge();
+        }
+
+        
+
+    }
+
+}
